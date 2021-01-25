1ff84d40856b9e31beed4dec441e25563b7894d4
Sasha Larin <sashalarin200@gmail.com>
2021-01-25 16:42:24 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.de9402671cf755f32035aa011bd599d5.js"
 );
 
 self.addEventListener('message', (event) => {
